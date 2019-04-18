f219579fc8d15b182180179ee42e83f885ed77f9
Rohmad Arifin <rohmadarifin007@gmail.com>
2019-04-18 23:57:10 +0700

    fix error condition if the countdown has stopped

--- index.js
+++ index.js
@@ -19,14 +19,53 @@ let fasting = setInterval(function() {
   let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  document.getElementById("days").innerHTML = days;
+  document.getElementById("days").innerHTML = `0${days}`.slice(-2);
   document.getElementById("hours").innerHTML = `0${hours}`.slice(-2);
   document.getElementById("minutes").innerHTML = `0${minutes}`.slice(-2);
   document.getElementById("seconds").innerHTML = `0${seconds}`.slice(-2);
 
   if (distance <= 0) {
     clearInterval(fasting);
-    document.getElementById("countdown").innerHTML = "Happy Ramadhan";
+    document.body.innerHTML = `<h1>Happy Fasting</h1>
+    <p>“Fasting men and fasting women, God has prepared forgiveness and a splendid wage.”</p>
+<p>Quran-Al Ahzab (35)</p>
+    <footer>
+        <span id="today"></span>
+        <p><a href="https://github.com/arifinoid">arifinoid</a> &copy; 2019. Indonesia.</p>
+    </footer>
+    
+<style>
+h1 {
+  padding-top: 120px;
+  color: gold;
+  font-size: 100px;
+  font-family: 'Spectral', serif;
+} 
+
+footer {
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+  font-size: 9px;
+  bottom: 0;
+  background-color: black;
+}
+
+footer p {
+  margin: 0 auto;
+}
+
+footer a:hover {
+  color: aqua;
+}
+
+footer a {
+  color: #fff;
+  text-decoration-line: none;
+}
+</style>`;
   }
 }, 1000);
 
